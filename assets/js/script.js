6cfa0e9b96d5e3a00bc952a0a21cee85a167ce7b
RITESH SOLANKI <riteshhsolankii@gmail.com>
2024-12-01 15:01:40 +0530

    sunday

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 // Humburger Menu Start
 
-let menuBtn = document.querySelector(".menu-bar-icon");
-let menuItems = document.querySelector(".header-mobile");
-let closeBtn = document.querySelector(".header-mobile-close");
+const menuBtn = document.querySelector(".menu-bar-icon");
+const menuItems = document.querySelector(".header-mobile");
+const closeBtn = document.querySelector(".header-mobile-close");
 
 menuBtn.addEventListener("click", function () {
   menuItems.classList.add("menu-show");
@@ -12,14 +12,28 @@ closeBtn.addEventListener("click", function () {
   menuItems.classList.remove("menu-show");
 });
 
-
 // Sticky Header Start
 
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
   const headerMain = document.querySelector("#header");
-  if(window.scrollY > 50){
+  if (window.scrollY > 50) {
     headerMain.classList.add("sticky");
   } else {
-    headerMain.classList.remove("sticky")
+    headerMain.classList.remove("sticky");
+  }
+});
+
+// Search Box Open
+
+const searchBtn = document.querySelector(".search-icon");
+const searchPanel = document.querySelector(".search-panel-main");
+
+searchBtn.addEventListener("click", () => {
+  searchPanel.classList.add("search-panel-show");
+});
+
+document.addEventListener("keydown", (escevent) => {
+  if (escevent.key === "Escape" || escevent.key === "Esc") {
+    searchPanel.classList.remove("search-panel-show");
   }
 });
